1ac7057b401789da4c95a81e25ac9d9d3614907d
Luke Nguyen <lukehaonguyen@gmail.com>
2019-09-24 13:49:29 -0600

    starting redux implementation with legacy DnD functions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import ReactDOM from "react-dom";
 import App from "./App";
 import Board from "./Board";
